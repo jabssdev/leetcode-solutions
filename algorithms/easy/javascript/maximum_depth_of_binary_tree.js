3cadc62a0ea8f8bda415ebe1d45245001811b6ab
JabssDev <jordanburgoss23@gmail.com>
2026-04-29 13:25:35 -0500

    feat: implement maximum depth of binary tree solution in PHP and JavaScript

---
+++ algorithms/easy/javascript/maximum_depth_of_binary_tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+	if (!root) return 0;
+
+	let depth = 0;
+	const queue = [root];
+
+	while (queue.length > 0) {
+		const levelSize = queue.length;
+		depth++;
+
+		for (let i = 0; i < levelSize; i++) {
+			const node = queue.shift();
+			if (node.left) queue.push(node.left);
+			if (node.right) queue.push(node.right);
+		}
+	}
+
+	return depth;
+};

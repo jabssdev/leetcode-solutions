553a46ad09145846cc9f4280ed70673ac71c0de8
JabssDev <jordanburgoss23@gmail.com>
2026-05-04 20:42:58 -0500

    feat: implement Path Sum algorithm in PHP and JavaScript

---
+++ algorithms/easy/php/path_sum.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $targetSum
+     * @return Boolean
+     */
+    function hasPathSum($root, $targetSum) {
+        if ($root === null) return false;
+
+        if ($root->left === null && $root->right === null) return $root->val === $targetSum;
+
+        $remainingSum = $targetSum - $root->val;
+
+        return $this->hasPathSum($root->left, $remainingSum) || $this->hasPathSum($root->right, $remainingSum);
+    }
+}

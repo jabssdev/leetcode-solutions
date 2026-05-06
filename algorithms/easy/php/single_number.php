d00d8f3fcae023f843f80c10c8ed23fd467f6e13
JabssDev <jordanburgoss23@gmail.com>
2026-05-05 21:52:30 -0500

    feat: add single number algorithm implementation in PHP and JavaScript

---
+++ algorithms/easy/php/single_number.php
@@ -0,0 +1,16 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function singleNumber($nums) {
+        $result = 0;
+
+		foreach ($nums as $num) $result ^= $num;
+
+		return $result;
+    }
+}

0156e00a502fe4851e1cde55ca4f33ba39e27e65
JabssDev <jordanburgoss23@gmail.com>
2026-05-10 23:51:13 -0500

    feat: add reverse bits solution in JavaScript and PHP

---
+++ algorithms/easy/php/reverse_bits.php
@@ -0,0 +1,18 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function reverseBits($n) {
+        $result = 0;
+        for ($i = 0; $i < 32; $i++) {
+            $result <<= 1;
+            $result |= $n & 1;
+            $n >>= 1;
+        }
+        return $result;
+    }
+}

f4f62b157e173d2648470eb5bd5033c7574ab656
JabssDev <jordanburgoss23@gmail.com>
2026-05-11 21:20:07 -0500

    feat: implement remove linked list elements solution in PHP and JavaScript

---
+++ algorithms/easy/php/remove_linked_list_elements.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @param Integer $val
+     * @return ListNode
+     */
+    function removeElements($head, $val) {
+        $dummy = new ListNode(0, $head);
+        $current = $dummy;
+
+        while ($current->next !== null) {
+            if ($current->next->val === $val) {
+                $current->next = $current->next->next;
+            } else {
+                $current = $current->next;
+            }
+        }
+
+        return $dummy->next;
+    }
+}

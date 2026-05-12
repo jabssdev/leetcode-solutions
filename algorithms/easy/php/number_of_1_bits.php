faba31a9aa0c9c63945a5f5262147bba8eda41bf
JabssDev <jordanburgoss23@gmail.com>
2026-05-11 20:39:49 -0500

    feat: add Number of 1 Bits solution in JavaScript and PHP

---
+++ algorithms/easy/php/number_of_1_bits.php
@@ -0,0 +1,19 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function hammingWeight($n) {
+        $count = 0;
+
+        while ($n !== 0) {
+            $n &= $n - 1;
+            $count++;
+        }
+
+        return $count;
+    }
+}

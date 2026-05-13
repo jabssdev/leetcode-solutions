6013947c8a9445263d481a781232cb402619461a
JabssDev <jordanburgoss23@gmail.com>
2026-05-12 23:54:23 -0500

    feat: add contains duplicate algorithm implementations in JavaScript and PHP

---
+++ algorithms/easy/php/contains_duplicate.php
@@ -0,0 +1,21 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function containsDuplicate($nums) {
+        $seen = [];
+
+        foreach ($nums as $num) {
+            if (isset($seen[$num])) {
+                return true;
+            }
+            $seen[$num] = true;
+        }
+
+        return false;
+    }
+}
